6a971dac9cd54f693628eba4b32955f6c65c5451
grtlinux <grtlinux@gmail.com>
2020-06-11 13:16:39 +0900

    create first

---
+++ ver-0.4/KieaJackson-02-group/src/main/java/org/tain/KieaJackson02GroupApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaJackson02GroupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaJackson02GroupApplication.class, args);
+	}
+
+}
