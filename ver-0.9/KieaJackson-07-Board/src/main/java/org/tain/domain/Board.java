85ef86f1dcada4414e7f0555b48f871811f2eb64
grtlinux <grtlinux@gmail.com>
2020-06-18 17:26:05 +0900

    update

--- ver-0.9/KieaJackson-07-Board/src/main/java/org/tain/domain/Board.java
+++ ver-0.9/KieaJackson-07-Board/src/main/java/org/tain/domain/Board.java
@@ -1,6 +1,7 @@
 package org.tain.domain;
 
 import java.time.LocalDateTime;
+import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -10,6 +11,10 @@ import javax.persistence.Id;
 import javax.persistence.Index;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.tain.annotation.ColumnPosition;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -36,27 +41,38 @@ public class Board {
 	@Id
 	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "board_seq")
 	@Column(name = "id")
+	@ColumnPosition(1)
 	@JsonIgnore
 	private Long id;
 	
 	@Column(name = "title", length = 256)
+	@ColumnPosition(2)
 	private String title;
 	
 	@Column(name = "sub_title", length = 256)
+	@ColumnPosition(3)
 	private String subTitle;
 	
 	@Column(name = "content", length = 1024)
+	@ColumnPosition(4)
 	private String content;
 	
 	@Column(name = "user_id", length = 32)
+	@ColumnPosition(5)
 	private String userId;
 	
 	@Column(name = "created_date")
+	@ColumnPosition(6)
 	//@JsonIgnore
 	//@JsonProperty(access = Access.WRITE_ONLY)
 	//@JsonFormat(shape = Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
 	private LocalDateTime createdDate = LocalDateTime.now();
 
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name = "job_date")
+	@ColumnPosition(7)
+	private Date jobDate;
+	
 	@Builder
 	public Board(
 			String title,
@@ -68,5 +84,6 @@ public class Board {
 		this.subTitle = subTitle;
 		this.content = content;
 		this.userId = userId;
+		this.jobDate = new Date();
 	}
 }
