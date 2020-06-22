cc1372e50999d4a6a0935149e2ed49abd6580e97
grtlinux <grtlinux@gmail.com>
2020-06-22 20:06:39 +0900

    create 1st

---
+++ ver-1.0/KieaJackson-10-LNSLink/src/main/java/org/tain/KieaJackson10LnsLinkApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaJackson10LnsLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaJackson10LnsLinkApplication.class, args);
+	}
+
+}
