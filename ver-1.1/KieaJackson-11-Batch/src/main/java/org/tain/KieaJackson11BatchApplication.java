fc596abca381c52edff5aff9b08f6291b3ad5d16
grtlinux <grtlinux@gmail.com>
2020-06-23 12:27:45 +0900

    create 1st

---
+++ ver-1.1/KieaJackson-11-Batch/src/main/java/org/tain/KieaJackson11BatchApplication.java
@@ -0,0 +1,13 @@
+package org.tain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KieaJackson11BatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KieaJackson11BatchApplication.class, args);
+	}
+
+}
